b58f4621acecae6763111707f727c769b1905a00
vishwas <vishwas.gupta@snapdeal.com>
2017-05-06 17:17:01 +0530

    test cases for parking, leaving and getSlotNumberByRegNumber passed.

--- src/main/java/com/parkinglot/service/ParkingLot.java
+++ src/main/java/com/parkinglot/service/ParkingLot.java
@@ -9,21 +9,44 @@ public class ParkingLot {
 
 	private int capacity;
 
+	private Ticket[] ticketArray;
+	
+	private TicketService ticketService;
+	
 	public ParkingLot(int capacity) {
 		super();
 		this.capacity = capacity;
+		this.ticketArray = new Ticket[capacity+1]; 
+		this.ticketService = new TicketService();
 	}
 	
 	public Ticket park(Vehicle vehicle){
-		return null;
+		Ticket ticket = ticketService.getTicketByRegNumber(vehicle.getRegistrationNumber());
+		if(ticket!=null){
+			return ticket;
+		}
+		int i = getNextEmptySlot();
+		if(i==-1){
+			return null;
+		}
+		ticketArray[i] = ticketService.generateNewTicket(i, vehicle);
+		return ticketArray[i];
 	}
 	
 	public void leave(int slotNo){
-		
+		if(slotNo<0 || slotNo > capacity){
+			return;
+		}
+		ticketService.destroyTicket(ticketArray[slotNo]);
+		ticketArray[slotNo] = null;
 	}
 	
 	public Integer getSlotNumberByRegNumber(String regNumber){
-		return null;
+		Ticket ticket = ticketService.getTicketByRegNumber(regNumber);
+		if(ticket == null){
+			return null;
+		}
+		return ticket.getSlotNo();
 	}
 	
 	public List<Integer> getSlotNumbersByColor(String color){
@@ -37,5 +60,14 @@ public class ParkingLot {
 	public String getStatus(){
 		
 		return null;
+	}
+	
+	private int getNextEmptySlot(){
+		for(int i=1;i<=capacity;i++){
+			if(ticketArray[i]==null){
+				return i;
+			}
+		}
+		return -1;
 	}
 }
---
+++ src/main/java/com/parkinglot/service/TicketService.java
@@ -0,0 +1,54 @@
+package com.parkinglot.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.parkinglot.entities.Ticket;
+import com.parkinglot.entities.Vehicle;
+
+class TicketService {
+
+	Map<String,Ticket> regNumberTicketMap;
+	
+	Map<String,List<Ticket>> colorTicketsMap;
+	
+	public TicketService() {
+		regNumberTicketMap = new HashMap<>();
+		colorTicketsMap = new HashMap<>();
+	}
+	
+	public Ticket generateNewTicket(Integer slot,Vehicle vehicle){
+		
+		Ticket ticket = new Ticket(slot, vehicle);
+		regNumberTicketMap.put(vehicle.getRegistrationNumber(),ticket);
+		updateColorTicketMap(vehicle, ticket);
+		return ticket;
+	}
+	
+	public Ticket getTicketByRegNumber(String regNumber){
+		return regNumberTicketMap.get(regNumber);
+	}
+	
+	public List<Ticket> getTicketsByColor(String color){
+		return colorTicketsMap.get(color);
+	}
+	
+	public void destroyTicket(Ticket ticket){
+		regNumberTicketMap.remove(ticket.getVehicle().getRegistrationNumber());
+		colorTicketsMap.get(ticket.getVehicle().getColor()).remove(ticket);
+	}
+	
+	private void updateColorTicketMap(Vehicle vehicle, Ticket ticket){
+		List<Ticket> colorTickets = colorTicketsMap.get(vehicle.getColor());
+		if(colorTickets!=null){
+			colorTickets.add(ticket);
+		}
+		else{
+			colorTickets = new ArrayList<>();
+			colorTickets.add(ticket);
+			colorTicketsMap.put(vehicle.getColor(), colorTickets);
+		}
+	}
+}
